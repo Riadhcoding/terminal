3edb1e35efec50fa3cadae9a8ea10557a58b5479
python life <71737254+python-life@users.noreply.github.com>
2021-01-30 00:06:21 +0000

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -1,4 +1,4 @@
-import os,sys,time
+import os, sys, time
 def jalan(z):
     for e in z + '\n':
         sys.stdout.write(e)
@@ -6,9 +6,8 @@ def jalan(z):
         time.sleep(00000.02)
 def clear():
     os.system('clear')
-clear()
-os.system('git pull https://github.com/python-life/terminal')
-clear()
+os.system('git pull')
+
 logo = """
 \033[1;34m                       dP   dP                         
 \033[1;34m                       88   88                         
@@ -18,7 +17,7 @@ logo = """
 \033[1;33m   88Y888P' `8888P88   dP   dP    dP `88888P' dP    dP 
 \033[1;34m   88            .88                                   
 \033[1;34m   dP        d8888P  
-         
+
 \033[1;34m   dP    8888b          
 \033[1;37m   88 o  88             
 \033[1;33m   88 dP 88aaa  .d8888b. 
@@ -26,44 +25,42 @@ logo = """
 \033[1;37m   88 88 88     88 \033[1;31m    Created by Riad
 \033[1;34m   dP dP dP     `88888P
 """
-class main:
-    def __init__(self):
-        print(logo)
-        self.list()
-    def list (self):
-        print("\033[1;31m[1] \033[1;34mRestore the default terminal")
-        time.sleep(0.3)
-        print("\033[1;31m[2] \033[1;34mMy Youtube")
-        time.sleep(0.3)
-        print("\033[1;31m[3] \033[1;34mMy instagram")
-        time.sleep(0.3)
-        print("\033[1;31m[0] \033[1;34mExit")
-        time.sleep(0.3)
-        print('')
-        choose = input("\033[1;31m[?]\033[1;37mChoose an option : ")
-        if choose == '1':
-            jalan("please wait...")
-            os.system('cd /data/data/com.termux/files/usr/etc;rm -rf bash.bashrc')
-            os.system('cd terminal ;cp bash.bashrc /data/data/com.termux/files/usr/etc')
-            clear()
-            print(logo)
-            print("\033[1;31mClose and open the application again")
-            time.sleep(0.5)
-            os.system('exit')
-        elif choose == '2':
-            os.system('xdg-open https://www.youtube.com/c/pythonlife')
-            clear()
-            print(logo)
-            time.sleep(0.3)
-            os.system('exit')
-        elif choose == '3':
-            os.system('xdg-open https://www.instagram.com/python.life')
-            clear()
-            print(logo)
-            time.sleep(0.3)
-            os.system('exit')
-        elif choose == '0':
-            clear()
-            print(logo)
-            os.system('exit')
-main()
+clear()
+print(logo)
+print("\033[1;31m{1} \033[1;34mRestore the default terminal")
+time.sleep(0.3)
+print("\033[1;31m{2} \033[1;34mMy Youtube")
+time.sleep(0.3)
+print("\033[1;31m{3} \033[1;34mMy instagram")
+time.sleep(0.3)
+print("\033[1;31m{0} \033[1;34mExit")
+time.sleep(0.3)
+print('')
+choose = input("\033[1;31m{?}\033[1;37mChoose an option : ")
+if choose == '1':
+    jalan("please wait...")
+    os.chdir('/data/data/com.termux/files/usr/etc')
+    os.remove('bash.bashrc')
+    os.chdir('$HOME')
+    os.system('cd terminal;cp bash.bashrc /data/data/com.termux/files/usr/etc')
+    clear()
+    print(logo)
+    os.system('termux-reload-settings')
+    print("\033[1;31mDefault Setting")
+    os.system('exit')
+elif choose == '2':
+    os.system('xdg-open https://www.youtube.com/c/pythonlife')
+    clear()
+    print(logo)
+    time.sleep(0.3)
+    os.system('exit')
+elif choose == '3':
+    os.system('xdg-open https://www.instagram.com/python.life')
+    clear()
+    print(logo)
+    time.sleep(0.3)
+    os.system('exit')
+elif choose == '0':
+    clear()
+    print(logo)
+    os.system('exit')
